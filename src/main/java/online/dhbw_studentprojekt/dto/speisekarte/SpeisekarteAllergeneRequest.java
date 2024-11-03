21f640cd13ff5643df0d6c5e19de76c97be684e7
fabiansig <fabian.sigmund1@gmail.com>
2024-11-03 11:18:07 +0100

    fix: rm date from SpeisekarteAllergeneRequest

--- src/main/java/online/dhbw_studentprojekt/dto/speisekarte/SpeisekarteAllergeneRequest.java
+++ src/main/java/online/dhbw_studentprojekt/dto/speisekarte/SpeisekarteAllergeneRequest.java
@@ -2,5 +2,5 @@ package online.dhbw_studentprojekt.dto.speisekarte;
 
 import java.util.List;
 
-public record SpeisekarteAllergeneRequest(String date, List<String> allergene) {
+public record SpeisekarteAllergeneRequest(List<String> allergene) {
 }
