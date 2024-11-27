19e8b2075aa01221e14dec1249bd8021f37e03cc
Fabian <fabian.sigmund1@gmail.com>
2024-11-27 08:42:20 +0100

    add: weather to wetter record

--- src/main/java/online/dhbw_studentprojekt/dto/wetter/Wetter.java
+++ src/main/java/online/dhbw_studentprojekt/dto/wetter/Wetter.java
@@ -1,11 +1,15 @@
 package online.dhbw_studentprojekt.dto.wetter;
 
+import java.util.List;
+
 public record Wetter(
         String name,
-        Main main
+        Main main,
+        List<Weather> weather
 ) {
     public record Main(double temp, double feels_like, double temp_min, double temp_max) {
     }
 
+    public record Weather(int id, String main, String description, String icon) {
+    }
 }
-
